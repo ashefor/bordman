fa1b1ac384da5901fc9b1e6f3ae95030fd98f5e2
ashefor <thatsureboy@gmail.com>
2019-04-11 03:07:50 +0100

    started work on dashboard UI & added betslip tray

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,12 +8,21 @@ import { AuthService } from '../services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
+  title = "ifunanya"
+  heroes = [];
+  odds = []
   constructor(private authservice: AuthService) { }
 
   ngOnInit() {
+    console.log(this.heroes)
   }
 
   logout(){
     this.authservice.SignOut()
+  }
+  addodds(event){
+    this.heroes.push(event.target.value);
+    console.log(this.heroes)
+    console.log(event.target.value)
   }
 }
